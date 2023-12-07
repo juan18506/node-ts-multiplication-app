e90767767975a50a5183dda3029eff666ab8c9fe
Juan Alves Pedreira <juan185067@gmail.com>
2023-12-07 01:41:03 -0300

    added yarg options

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,9 @@
 import { yarg as argv } from './config/plugins/args.plugin';
 
-// console.log(process.argv);
-console.log(argv);
+(async () => {
+  await main();
+})();
+
+async function main() {
+  console.log(argv);
+}
